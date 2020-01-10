6df5c5fc837f1c6e60a954aa03ade0ca15a28b1d
Madison Randle <madisonrandle25@gmail.com>
2020-01-10 16:19:25 -0700

    Create function that checks and clears new task input field

--- main.js
+++ main.js
@@ -1,21 +1,39 @@
-var newListSection = document.querySelector('#left-side-wrapper');
+var newTaskInput = document.querySelector('#task-item-input');
+var newTaskBtn = document.querySelector('#add-task-btn');
 
-newListSection.addEventListener('click', addNewTask);
+newTaskInput.addEventListener('keyup', newListHandler);
 
+function newListHandler() {
+  checkForNewTaskInput();
+}
+
+// ADDS A NEW TASK TO THE LIST BEING CREATED
 function addNewTask() {
   var taskBox = document.querySelector('.add-task-holder');
-  var addTaskBtn = document.querySelector('#add-task-btn');
-
-
-  if (event.target.id.includes('add-task-btn')) {
-    taskBox.classList.remove('hidden');
-    taskBox.insertAdjacentHTML('afterbegin', `
-    <section class="new-task-wrapper">
-        <img class="close-img-btn" src="./assets/delete.svg" alt="close icon">
-      <p class="task-text new-task-font">Every chance I get, I water the plants.</p>
-    </section>
-    `);
+  taskBox.classList.remove('hidden');
+  taskBox.insertAdjacentHTML('afterbegin', `
+  <section class="new-task-wrapper">
+      <img class="close-img-btn" src="./assets/delete.svg" alt="close icon">
+    <p class="task-text new-task-font">${newTaskInput.value}</p>
+  </section>
+  `);
+  newTaskBtn.disabled = true;
+  clearInputField();
   }
+
+// THIS CHECKS FOR SOMETHING IN THE INPUT AND ENABLES / DISABLES THE NEW TASK BTN DEPENDING
+function checkForNewTaskInput() {
+  newTaskBtn.addEventListener('click', addNewTask);
+  newTaskInput.value !== '' ? newTaskBtn.disabled = false : newTaskBtn.disabled = true;
 }
+
+// THIS CLEARS THE INPUT FIELDS
+function clearInputField() {
+  var inputField = document.querySelector('#task-form');
+  inputField.reset();
+}
+
+
+
 
 //
