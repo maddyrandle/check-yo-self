865d90ad9930286ccb95fa1b15edf8e86c1259cb
Madison Randle <madisonrandle25@gmail.com>
2020-01-13 23:25:48 -0700

    Change variable, key, and function names

--- main.js
+++ main.js
@@ -1,228 +1,254 @@
+var toDoListsWrapper = document.querySelector('.left-side');
+var formWrapper = document.querySelector('.form-wrapper');
+var tasksWrapperOnForm = document.querySelector('.task-form-container');
+var formInputs = document.querySelector('form');
 var titleInput = document.querySelector('#title-input');
 var taskInput = document.querySelector('#task-input');
-var taskHolder = document.querySelector('.task-form-container');
-var taskInputContainer = document.querySelector('.task-input-wrapper');
-var formContainer = document.querySelector('.form-wrapper');
-var leftListHolder = document.querySelector('.left-side');
-var noListMsg = document.querySelector('.make-list-msg');
-var tasks = [];
-var lists = [];
-var list;
-var activeStyle;
+var taskObjsArray = [];
+var toDosArray = [];
+var toDoList;
+var completedTaskCSS;
 
-taskInput.addEventListener('keyup', enableAndDisableButtons);
-titleInput.addEventListener('keyup', enableAndDisableButtons);
-taskHolder.addEventListener('click', removeTask);
-taskInputContainer.addEventListener('click', collectTaskInfo);
-formContainer.addEventListener('click', collectFormInfo);
-leftListHolder.addEventListener('click', removeList);
+formInputs.addEventListener('keyup', disableBtns);
+tasksWrapperOnForm.addEventListener('click', removeTask);
+toDoListsWrapper.addEventListener('click', removeList);
+formWrapper.addEventListener('click', targetNewTaskDescription);
 window.addEventListener('load', pageLoad);
 
-
-
-
-// function showTaskComplete() {
-//   var tasksArray;
-//   var eventId = parseInt(event.target.getAttribute('id'));
-//   var inactive = document.querySelector('.inactive-checkbox');
-//   var active = document.querySelector('.active-checkbox');
-//
-//   for (var i = 0; i < lists.length; i++) {
-//
-//     tasksArray = lists[i].tasks;
-//
-//     for (var j = 0; j < tasksArray.length; j++) {
-//       if (tasksArray[j].id === eventId) {
-//         tasksArray[j].completed = true;
-//         event.target.classList.add('hidden');
-//         active.classList.remove('hidden');
-//         styleActiveTask();
-//       }
-//     }
-//   }
-//
-//   list.saveToStorage(lists);
-// }
-//
-// function styleActiveTask() {
-//   var text = document.querySelector('.task-text-p');
-//   activeStyle = text.classList.add('task-text-active');
-//
-// }
-
-// COLLECTS TASK INFO FROM THE FORM AND BEGINS PROCESS
-// OF INSTANTIATING A NEW TASK
-function collectTaskInfo() {
-  if (event.target.className === 'new-task-btn') {
-  createNewTask(taskInput.value, false);
-  }
-}
-
-// INSTANTIATES A NEW TASK
-function createNewTask(item, complete) {
-  var task = new Task(item, complete);
-  tasks.push(task);
-  task.addNewTask(task);
-  taskInput.value = '';
-  enableAndDisableButtons();
-}
-
-// COLLECTS TITLE INFO FROM FORM AND BEGINS PROCESS OF
-// INSTANTIATING A NEW TODO LIST
-function collectFormInfo() {
-  if (event.target.className === 'new-list-btn') {
-  makeList(titleInput.value, tasks, false);
-  taskHolder.innerText = '';
-  hideTaskContainer();
-  }
-}
-
-// INSTANTIATES A NEW TODO LIST
-function makeList(title, tasks, urgent) {
-  list = new ToDoList(title, tasks, urgent);
-  lists.push(list);
-  list.addNewList(list);
-  clearInputField();
-  enableAndDisableButtons();
-  list.saveToStorage(lists);
-}
-
-// STARTS PROCESS OF BRINGING BACK KEY:LIST FROM LOCALSTORAGE
 function pageLoad() {
-  if ('list' in localStorage) {
-    checkLocalStorage();
+  'ToDoList' in localStorage ? inspectLocalStorage() : '';
+}
+
+function inspectLocalStorage() {
+  var storedToDosArray = parseLocalStorage();
+  for (var i = 0; i < storedToDosArray.length; i++) {
+    storedToDosArray[i].tasks.forEach(j => taskObjsArray.push(j));
+    instantiateToDoList(storedToDosArray[i].title, storedToDosArray[i].tasks);
   }
 }
 
-// GETS KEY:LIST FROM LOCALSTORAGE AND TAKES IT OUT OF STRING
 function parseLocalStorage() {
-  var getListItem = localStorage.getItem('list');
-  var storedArray = JSON.parse(getListItem);
-  return storedArray;
+  var storedToDosArray = localStorage.getItem('ToDoList');
+  var parsedToDosArray = JSON.parse(storedToDosArray);
+  return parsedToDosArray;
 }
 
-// LOOPS THROUGH ARRAY OF TODO LISTS IN LOCALSTORAGE AND
-// MAKES A CARD WITH THE STORED PROPERTIES
-function checkLocalStorage() {
-  var storedListArray = parseLocalStorage();
-  for (var i = 0; i < storedListArray.length; i++) {
-    storedListArray[i].tasks.forEach(j => tasks.push(j));
-    makeList(storedListArray[i].title, storedListArray[i].tasks, storedListArray[i].urgent);
-  }
-}
+// function showUrgentStyling() {
+//   var urgentStyle = event.target.nextElementSibling;
+//   var urgentCard = event.target.closest('.card');
+//   var urgentHeader = document.querySelector('.card-title');
+//   var urgentFooter = document.querySelector('.card-footer');
+//   event.target.classList.contains('.card');
+//   .add('urgent-card');
+//   event.target.urgentHeader.classList
+//   .add('urgent-card-title');
+//   event.target.urgentStyle.classList
+//   .add('urgent-footer-font');
+//   event.target.urgentFooter.classList
+//   .add('urgent-card-footer');
+//
+// }
 
-// LOOPS THROUGH TASK ARRAY AND MATCHES IT TO THE ID OF
-// THE CLOSEST TASK TO THE X BUTTON CLICKED
-function findTaskId() {
-  var taskId = parseInt(event.target.closest('.close-img-btn').id);
-  for (var i = 0; i < tasks.length; i++) {
-    if (tasks[i].id === taskId) {
-      return tasks[i];
+function designateTaskCompleted() {
+  var eventTaskId = parseInt(event.target.id);
+  var toDoTasksArray;
+  for (var i = 0; i < toDosArray.length; i++) {
+    toDoTasksArray = toDosArray[i].tasks;
+    for (var j = 0; j < toDoTasksArray.length; j++) {
+      if (toDoTasksArray[j].id === eventTaskId) {
+        toDoTasksArray[j].completed = true;
+        revealCompletedTask();
+      }
     }
   }
+
+  toDoList.saveToStorage(toDosArray);
 }
 
-function findListId() {
-  var findCard = event.target.closest('.card');
-  var listId = parseInt(findCard.id);
-  for (var i = 0; i < lists.length; i++) {
-    if (lists[i].id === listId) {
-      return lists[i];
-    }
+function revealCompletedTask() {
+  var taskDescription = document.querySelector('.task-text-p');
+  completedTaskCSS = taskDescription.classList.add('task-text-active');
+}
+
+function targetNewTaskDescription() {
+  if (event.target.className === 'new-task-btn') {
+  instantiateTask(taskInput.value);
+  }
+
+  targetNewToDoListValues();
+}
+
+function instantiateTask(description) {
+  if (event.target.className === 'new-task-btn') {
+    var task = new Task(description);
+    tasksWrapperOnForm.classList.remove('hidden');
+    tasksWrapperOnForm.insertAdjacentHTML('beforeend', `
+    <section id="${task.id}" class="new-task-wrapper">
+      <img class="close-img-btn" src="./assets/delete.svg" alt="close icon">
+      <p class="task-text new-task-font">${description}</p>
+    </section>
+    `);
+  }
+
+  taskObjsArray.push(task);
+  taskInput.value = '';
+  disableBtns();
+}
+
+function targetNewToDoListValues() {
+  if (event.target.className === 'new-list-btn') {
+  instantiateToDoList(titleInput.value, taskObjsArray);
+  tasksWrapperOnForm.innerText = '';
+  hideTasksWrapperOnForm();
   }
 }
 
-// REMOVES LIST FROM ARRAY IN LOCALSTORAGE
-// REMOVES LIST FROM THE FORM ON THE PAGE
-function removeList() {
-  var listToRemove = findListId(event);
-  var i = lists.indexOf(listToRemove);
-  if (event.target.parentNode.classList.contains('delete-btn')) {
-    lists.splice(i, 1);
-    event.target.closest('.card').remove();
-    listToRemove.saveToStorage(lists);
-  }
+function instantiateToDoList(title, tasks) {
+  toDoList = new ToDoList(title, tasks);
+  toDosArray.push(toDoList);
+  displayNewToDoList(toDoList);
+  formInputs.reset();
+  disableBtns();
+  toDoList.saveToStorage(toDosArray);
 }
 
-// REMOVES TASK FROM ARRAY IN LOCALSTORAGE
-// REMOVES TASK FROM THE FORM ON THE PAGE
-function removeTask() {
-  var taskToRemove = findTaskId(event);
-  var i = tasks.indexOf(taskToRemove);
-  if (event.target.className === 'close-img-btn') {
-    tasks.splice(i, 1);
-    event.target.closest('.new-task-wrapper').remove();
-  }
-  hideTaskContainer();
+function displayNewToDoList(toDoList) {
+  var msgToUser = document.querySelector('.make-list-msg');
+  var checkboxId = Date.now().toString(36);
+  msgToUser.classList.add('hidden');
+  toDoListsWrapper.classList.remove('hidden');
+  toDoListsWrapper.insertAdjacentHTML('afterbegin', `
+  <div id="${toDoList.id}" class="card regular-card">
+    <header class="card-header">
+      <h1 class="card-title regular-card-title">${toDoList.title}</h1>
+    </header>
+    <main class="card-main">
+      <section class="all-tasks">
+      </section>
+    </main>
+    <footer class="card-footer regular-card-footer">
+      <div class="urgent-img-wrapper img-wrapper">
+        <input id="${checkboxId}" type="checkbox"/>
+        <label for="${checkboxId}"></label>
+        <p class="regular-footer-font">URGENT</p>
+      </div>
+      <div class="delete-img-wrapper img-wrapper">
+        <button class="delete-btn">
+          <img class="img-btn delete" src="./assets/delete.svg" alt="close icon">
+          <img class="img-btn delete hidden" src="./assets/delete-active.svg" alt="close icon">
+          <p class="regular-footer-font">DELETE</p>
+        </button>
+      </div>
+    </footer>
+  </div>
+  `);
+  displayTasksOnToDoList();
+  taskObjsArray = [];
 }
 
-// REMOVES ALL TASKS FROM ARRAY WHEN CLEAR BUTTON IS CLICKED
-function removeAllTasksFromArray() {
-  if (event.target.className === 'clear-btn') {
-      tasks.splice(0, tasks.length);
-  }
-}
-
-// REMOVES EMPTY CONTAINER WHERE TASKS ARE ADDED ON THE
-// PAGE, IF NO TASKS ARE ADDED ON THE FORM
-function hideTaskContainer() {
-  if (taskHolder.innerText === '' || tasks.length === 0) {
-    taskHolder.classList.add('hidden');
-  }
-}
-
-function clearAllBtn() {
-  if (event.target.className === 'clear-btn') {
-    taskHolder.innerText = '';
-    clearInputField();
-    hideTaskContainer();
-    removeAllTasksFromArray();
-  }
-}
-
-// CHECKS INPUT FIELDS AND ENABLES OR DISABLES BUTTONS ON
-// THE FORM
-function enableAndDisableButtons() {
-  var noTaskInput = taskInput.value === '';
-  var noTitleInput = titleInput.value === '';
-  var noTasks = taskHolder.classList.contains('hidden');
-  var newListBtn = document.querySelector('.new-list-btn')
-  var newTaskBtn = document.querySelector('.new-task-btn');
-  var clearBtn = document.querySelector('.clear-btn');
-  noTaskInput ? newTaskBtn.disabled = true : newTaskBtn.disabled = false;
-  noTitleInput || noTasks ? newListBtn.disabled = true : newListBtn.disabled = false;
-  if (noTasks || noTitleInput) {
-    clearBtn.disabled = true;
-  } else {
-    clearBtn.disabled = false;
-    formContainer.addEventListener('click', clearAllBtn);
-  }
-}
-
-// CLEARS THE INPUT FIELDS ON THE FORM
-function clearInputField() {
-  var inputField = document.querySelector('form');
-  inputField.reset();
-}
-
-function addTasksToList() {
-  var taskItem;
+function displayTasksOnToDoList() {
+  var taskDescription;
   var taskId;
-  var taskHolder = document.querySelector('.all-tasks');
-  for (var i = 0; i < tasks.length; i++) {
-    taskItem = tasks[i].item;
-    taskId = tasks[i].id;
-    taskHolder.insertAdjacentHTML('beforeend', `
+  var taskWrapper = document.querySelector('.all-tasks');
+  for (var i = 0; i < taskObjsArray.length; i++) {
+    taskDescription = taskObjsArray[i].description;
+    taskId = taskObjsArray[i].id;
+    taskWrapper.insertAdjacentHTML('beforeend', `
     <div class="single-task-wrapper">
       <div class="checkbox-img-wrapper">
         <input id="${taskId}" type="checkbox"/>
         <label for="${taskId}"></label>
       </div>
-      <p class="task-text-p">${taskItem}</p>
+      <p class="task-text-p">${taskDescription}</p>
     </div>
     `);
-    var completeTask = document.getElementById(`${taskId}`);
-    completeTask.checked = tasks[i].completed
+    // var completeTask = document.getElementById(`${taskId}`);
+    // completeTask.checked = tasks[i].completed;
     // ON CLICK MAKE TASK COMPLETED TRUE
   }
 }
+
+function matchTaskId() {
+  var eventTaskId = parseInt(event.target.closest('.close-img-btn').id);
+  for (var i = 0; i < taskObjsArray.length; i++) {
+    if (taskObjsArray[i].id === eventTaskId) {
+      return taskObjsArray[i];
+    }
+  }
+}
+
+function removeTask() {
+  var taskToRemove = matchTaskId(event);
+  var i = taskObjsArray.indexOf(taskToRemove);
+  if (event.target.className === 'close-img-btn') {
+    event.target.closest('.task-wrapper').remove();
+    taskObjsArray.splice(i, 1);
+  }
+
+  hideTasksWrapperOnForm();
+}
+
+function matchListId() {
+  var eventCard = event.target.closest('.card');
+  var eventListId = parseInt(eventCard.id);
+  for (var i = 0; i < toDosArray.length; i++) {
+    if (toDosArray[i].id === eventListId) {
+      return toDosArray[i];
+    }
+  }
+}
+
+function removeList() {
+  var listToRemove = matchListId(event);
+  var i = toDosArray.indexOf(listToRemove);
+  designateTaskCompleted();
+  if (event.target.parentNode.classList.contains('delete-btn')) {
+    event.target.closest('.card').remove();
+    toDosArray.splice(i, 1);
+    listToRemove.saveToStorage(toDosArray);
+  }
+}
+
+function hideTasksWrapperOnForm() {
+  if (tasksWrapperOnForm.innerText === '' || taskObjsArray.length === 0) {
+    tasksWrapperOnForm.classList.add('hidden');
+  }
+}
+
+function resetForm() {
+  if (event.target.className === 'clear-btn') {
+    tasksWrapperOnForm.innerText = '';
+    formInputs.reset();
+    hideTasksWrapperOnForm();
+    emptyTasksArray();
+  }
+}
+
+function emptyTasksArray() {
+  if (event.target.className === 'clear-btn') {
+    taskObjsArray.splice(0, taskObjsArray.length);
+  }
+}
+
+function disableBtns() {
+  var noTasks = tasksWrapperOnForm.classList.contains('hidden');
+  var newListBtn = document.querySelector('.new-list-btn')
+  var clearBtn = document.querySelector('.clear-btn');
+  if (titleInput.value === '' || noTasks) {
+    newListBtn.disabled = true;
+    clearBtn.disabled = true;
+  } else {
+    newListBtn.disabled = false;
+    clearBtn.disabled = false;
+    formWrapper.addEventListener('click', resetForm);
+  }
+
+  disableTaskBtn();
+}
+
+function disableTaskBtn() {
+  var newTaskBtn = document.querySelector('.new-task-btn');
+  taskInput.value === '' ? newTaskBtn.disabled = true : newTaskBtn.disabled = false;
+}
+
+//
