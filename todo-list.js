b4ff602b84a928504dc8ff80331f195acdb5700f
Madison Randle <madisonrandle25@gmail.com>
2020-01-13 23:27:10 -0700

    Change variable, key, and function names

--- todo-list.js
+++ todo-list.js
@@ -1,13 +1,13 @@
 class ToDoList {
-  constructor(title, tasks, urgent) {
+  constructor(title, tasks) {
     this.id = Date.now();
     this.title = title;
     this.tasks = tasks;
-    this.urgent = urgent;
+    this.urgent = false;
   }
 
   saveToStorage() {
-    localStorage.setItem('list', JSON.stringify(lists));
+    localStorage.setItem('ToDoList', JSON.stringify(toDosArray));
   }
 
   deleteFromStorage() {
@@ -16,48 +16,11 @@ class ToDoList {
 
   updateToDo() {
     // should update the todo’s title and urgency
+    this.urgent = !this.urgent;
   }
 
   updateTask() {
     // should update a task’s content and if it has been completed
-    // localStorage.setItem('active style', JSON.stringify(activeStyle));
-  }
-
-  addNewList(newList) {
-    var checkboxId = Date.now().toString(36);
-    noListMsg.classList.add('hidden');
-    leftListHolder.classList.remove('hidden');
-    leftListHolder.insertAdjacentHTML('afterbegin', `
-    <div id="${newList.id}" class="card regular-card">
-      <header class="card-header">
-        <h1 class="card-title regular-card-title">${newList.title}</h1>
-      </header>
-      <main class="card-main">
-        <section class="all-tasks">
-        </section>
-      </main>
-      <footer class="card-footer regular-card-footer">
-
-        <div class="urgent-img-wrapper img-wrapper">
-
-          <input id="${checkboxId}" type="checkbox"/>
-          <label for="${checkboxId}"></label>
-          <p class="regular-footer-font">URGENT</p>
-
-        </div>
-
-        <div class="delete-img-wrapper img-wrapper">
-          <button class="delete-btn">
-            <img class="img-btn delete" src="./assets/delete.svg" alt="close icon">
-            <img class="img-btn delete hidden" src="./assets/delete-active.svg" alt="close icon">
-            <p class="regular-footer-font">DELETE</p>
-          </button>
-        </div>
-      </footer>
-    </div>
-    `);
-    addTasksToList();
-    tasks = [];
   }
 }
 
