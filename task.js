1e21c5c4ad2bf058215e9e22fbc41e722dfcf30d
Madison Randle <madisonrandle25@gmail.com>
2020-01-13 23:26:54 -0700

    Change variable, key, and function names

--- task.js
+++ task.js
@@ -1,22 +1,8 @@
 class Task {
-  constructor(item, complete) {
+  constructor(description) {
     this.id = Date.now();
-    this.item = item;
-    this.completed = complete;
-  }
-
-  addNewTask(newTask) {
-    taskHolder.classList.remove('hidden');
-    taskHolder.insertAdjacentHTML('beforeend', `
-    <section id="${newTask.id}" class="new-task-wrapper">
-      <img class="close-img-btn" src="./assets/delete.svg" alt="close icon">
-      <p class="task-text new-task-font">${newTask.item}</p>
-    </section>
-    `);
-
+    this.description = description;
+    this.completed = false;
   }
 }
-
-
-
 //
